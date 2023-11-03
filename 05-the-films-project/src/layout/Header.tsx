f8cbc4e6bde1aa2220b01799f761d16e1790778f
dsibi <dasibirin@gmail.com>
2023-11-03 10:40:30 +0300

    links corrected

--- 05-the-films-project/src/layout/Header.tsx
+++ 05-the-films-project/src/layout/Header.tsx
@@ -2,12 +2,14 @@ function Header() {
   return (
     <nav className="#ef6c00 orange darken-3">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Movies
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a href="https://github.com/dsibi/react-for-modern-web-applications/tree/gh-pages">
+              Repo
+            </a>
           </li>
         </ul>
       </div>
